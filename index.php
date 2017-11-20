9d975e39ab4b6a1a45bc50223de839a7faaa2310
zonaylc <33519861+zonaylc@users.noreply.github.com>
2017-11-20 23:15:08 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="css/index_style.css" rel="stylesheet" type="text/css">
+  <link href="index_style.css" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -75,7 +75,6 @@ div.desc { /* Add desc...區塊的大小*/
 	  header('Location:login.php');
   }
   else{
-       $uid=$_SESSION['uid'];
 	   echo $_SESSION['uname'].'您好!'.'<a href="logout.php">登出</a>' ;
   }
 ?>

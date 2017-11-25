948041c321c1f75ec76b288d2e568812daedcbeb
dcw102213006 <dcw102213006@gmail.com>
2017-11-25 22:55:50 +0800

    update index.php
    修改搜尋功能...
    請大家先pull這支下來我不想再重複一直改超連結什麼的QQ

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react.min.js"></script>
   <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
-
+  <script src="Select_Object_Content.js"></script>
 
 
 <style>
@@ -105,7 +105,7 @@ div.desc { /* Add desc...區塊的大小*/
 		<div class="w3-dropdown-hover profile">
         <img class="w3-circle w3-image w3-xlarge" src="head.png" style="width:64" height="64">
         <div class="w3-dropdown-content w3-bar-block w3-border">
-        <a href="#" class="w3-bar-item w3-button">個人檔案</a>
+        <a href="profile.php?id=<?php echo $uid; ?>" class="w3-bar-item w3-button">個人檔案</a>
         <a href="#" class="w3-bar-item w3-button">我的願望清單</a>
         </div>
         </div>
@@ -119,21 +119,21 @@ div.desc { /* Add desc...區塊的大小*/
       <div class="btn-group" role="group">
         <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B1" data-toggle		="dropdown" aria-expanded="false">服飾<span class="caret"></span></button>
         <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
-          <li><a href="index.php?clo">女生</a></li>
-          <li><a href="#">男生</a></li>
+          <li><a href="#" onclick="select(1)">女生</a></li>
+          <li><a href="#" onclick="select(2)">男生</a></li>
         </ul>
       </div>
       <div class="btn-group" role="group">
         <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B2" data-toggle="dropdown" aria-expanded="false">鞋類<span class="caret"></span></button>
         <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
-          <li><a href="#">女生</a></li>
-          <li><a href="#">男生</a></li>
+          <li><a href="#" onclick="select(3)">女生</a></li>
+          <li><a href="#" onclick="select(4)">男生</a></li>
         </ul>
       </div>
-      <button type="button" class="btn btn-default B3">生活用品</button>
-      <button type="button" class="btn btn-default B4">3C</button>
-      <button type="button" class="btn btn-default B5">書本</button>
-      <button type="button" class="btn btn-default B6">文具</button>
+       <button type="button" class="btn btn-default B3"  onclick="select(5)">生活用品</button>
+      <button type="button" class="btn btn-default B4" onclick="select(6)">3C</button>
+      <button type="button" class="btn btn-default B5" onclick="select(7)">書本</button>
+      <button type="button" class="btn btn-default B6" onclick="select(8)">文具</button>
     </div>
     
     
@@ -175,7 +175,9 @@ div.desc { /* Add desc...區塊的大小*/
 
 
 <script>
-
+function select(type){
+    document.location.href="index.php?type="+type;
+}
    
 </script>
 

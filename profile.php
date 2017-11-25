f04506a311b7e2d8cc693e388e1c0ef82b815e8d
zonaylc <33519861+zonaylc@users.noreply.github.com>
2017-11-25 18:27:42 +0800

    個人檔案

--- profile.php
+++ profile.php
@@ -1,142 +1,158 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Personal Page</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link href="css/index_style.css" rel="stylesheet" type="text/css">
   <link href="css/img_modal.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	  
-  <script src="fileupload.js"></script>
-  <script src="dialog.js"></script>
+  <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react.min.js"></script>
+  <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
+  <script src="Select_Object_Content.js"></script>
+
+
+
+
+<style>
+div.gallery {
+    margin: 2%;
+    border: 1px solid #ccc;
+    float: left;
+    width: 250px;
+    height:250px;
+}
+
+div.gallery:hover {
+    border: 10px solid #FFF;
+}
+
+div.gallery img {
+    width: 240px;
+    height: 240px;
+    margin-top:5px;
+}
+
+div.desc { /* Add desc...區塊的大小*/
+    padding: 15px;
+    text-align: center;
+	background-color: #FCFCFC
+}
+#wall{
+    width:80%;
+}
+* {
+    box-sizing: border-box;
+}
+
+.responsive {
+    padding: 0 6px;
+    float: left;
+    width: 30%;
+}
+
+@media only screen and (max-width: 893px){
+    .responsive {
+        width: 49.99999%;
+        margin: 6px 0;
+    }
+}
+
+@media only screen and (max-width: 500px){
+    .responsive {
+        width: 100%;
+    }
+    .modal-content{
+        clear:left;
+        margin-right:100px;
+    }
+}
+
+.clearfix:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+</style>
 </head>
 <body class="body">
+
+<div class="w3-container w3-blue-gray topbar">
+	<form action="">
+    <img src="title.png" alt="" width="198" height="84" class="tp" />
+  	<input id="search-box" type="text" name="search-box"/>
+    </form>
+    
+<aside class="loginside" align="right">
 <?php
 
   require 'Oracle_connect.php';
- 
+  
   if(!isset($_SESSION['uname'])){//若使用者尚未登入
+      
 	  header('Location:login.php');
   }
   else{
-	   echo '<td align="center">'.$_SESSION['uname'].'您好!'.'<a href="logout.php" >登出</a>' ;
+       $uid=$_SESSION['uid'];
+	   echo $_SESSION['uname'].'您好!'.'<a href="logout.php">登出</a>' ;
   }
 ?>
-
-
-<form id="uploadForm" action="upload.php" method="post">
-<label>Upload Image File:</label><br/>
-<input name="fileToUpload" type="file" class="inputFile"  id="fileToUpload"  accept="image/*"/>
-<input type="submit" value="Submit" class="btnSubmit" id="img_submit"/>
-</form>
-
-
-
-<div id="img_area">
-<img id="img" src="">
-<button onclick="document.getElementById('id01').style.display='block'" id="next_btn" type="button" class="btn btn-success" style="display:none">下一步</button>
-
+</aside>
 </div>
 
-<div id="myModal" class="modal">
-  <span class="close">&times;</span>
-  <img class="modal-content" id="img01">
-  <div id="caption"></div>
-  
+<div class="container selfdesc">
+<img src="head.png" alt="headsticker" id="head"/> <br>
+	<div class="container desc-text" align="center">
+    <?php
+	echo $_SESSION['uname'];
+    ?><br>
+    我是李帥，很醜
+    </div> 
 </div>
-
-
  
-
-<div class="w3-container">
+  
+<div id="wall" align="center">
+      
+</div>
+  
+      
+</div>
+  
   
 
-  
-
-  <div id="id01" class="w3-modal">
-    <div class="w3-modal-content w3-card-4">
-      <header class="w3-container w3-teal"> 
-        <span onclick="document.getElementById('id01').style.display='none'" 
-        class="w3-button w3-display-topright">&times;</span>
-        <h2>發布貼文</h2>
-      </header>
-      <div class="w3-container">
-	        <div id="modal_fo">
-	        物品名稱:<input type="text" size="10" id="obj_name"><br>
-          物品種類:<form action="insert.php" method='post'><select name="obj_category" id="obj_category">
-                      <option selected value="DEFAULT">幫這個物品分類一下吧</option>
-                      <option value="1">服飾/女生</option>
-                      <option value="2">服飾/男生</option>
-                      <option value="3">鞋類/女生</option>
-                      <option value="4">鞋類/男生</option>
-                      <option value="5">生活用品</option>
-                      <option value="6">3C</option>
-                      <option value="7">書本</option>
-                      <option value="8">文具</option>
-                   </select>
-                   </form>
-                  
-                
-                   <div></div>
-          
-          <textarea rows="30" cols="90" placeholder="對這個物品寫點敘述吧" id="obj_dec" name="obj_dec"></textarea>
-                
-            
-		    <button onclick="declare()" style="float:right" type="button" class="btn btn-info">發布</button>
-			</div>
-      </div>
-	  <div id="mes" style="display:none">
-			    <div class="alert alert-success" >
-                   <strong>Success!</strong> 發布貼文成功!
-                </div>
-	  </div>
-      <footer class="w3-container w3-teal">
-        <p>Modal Footer</p>
-      </footer>
+      
     </div>
+  
+
   </div>
 </div>
 
 
+<div id="myModal" class="modal"  >
+  <span class="close">&times;</span>
+  <div class="modal-content" id="text_area" style="">
+    <img class="modal-content" id="img01" style="float:left"  >
+    
+  </div>
+  
+  
+ 
+  
+</div>
 
+<div class="fixed w3-container post" align="center">
+  <a href="post.php?id=<?php echo $uid?>"><button class="button" >POST</button></a>
+</div>
 </body>
 
 
 <script>
-function declare(){
-	 
-	 
-	$("#img_submit").submit();//上傳圖片
-	 
-	
-	
-	
-	
-	
-	
-	
-     
-}
 
-$(document).ready(function(){
-
-   $( "#next_btn" ).click(function() {
-       $("#modal_fo").show();
-	   $("#mes").hide();
-   });
-
-});
-
-
-
-
+   
 </script>
-
-
 
 
 

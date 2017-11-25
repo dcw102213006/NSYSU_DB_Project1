782cdc2e684c2af12a7781048479b99a9423b26f
zonaylc <33519861+zonaylc@users.noreply.github.com>
2017-11-25 22:05:51 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react.min.js"></script>
   <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
-  <script src="Select_Object_Content.js"></script>
-
 
 
 
@@ -80,76 +78,65 @@ div.desc { /* Add desc...區塊的大小*/
 </head>
 <body class="body">
 
-<div class="w3-container w3-blue-gray topbar">
-	<form action="">
-    <img src="title.png" alt="" width="198" height="84" class="tp" />
-  	<input id="search-box" type="text" name="search-box"/>
-    </form>
-     
+<div class="w3-container w3-blue-gray topbar" align="center">
     
-<aside class="loginside" align="right">
-<?php
-
-  require 'Oracle_connect.php';
+    <div class="right">
+	<?php
+  	require 'Oracle_connect.php';
   
-  if(!isset($_SESSION['uname'])){//若使用者尚未登入
+  	if(!isset($_SESSION['uname'])){//若使用者尚未登入
       
 	  header('Location:login.php');
-  }
-  else{
+  		}
+  	else{
        $uid=$_SESSION['uid'];
 	   echo $_SESSION['uname'].'您好!'.'<a href="logout.php">登出</a>' ;
-  }
-?>
-</aside>
+  		}
+	?>  
+	</div>
+    
+    <form action="">
+    <img src="title.png" alt="" width="106" height="42" class="tp" />
+  	<input id="search-box" type="text" name="search-box"/>
+    </form>
 </div>
 
-
-  <div class="w3-container" align="right" style="margin-top:20px">
-		<div class="w3-dropdown-hover" >
-        <img class="w3-btn w3-circle w3-image w3-jumbo" src="img_fjords.jpg" >
+<div class="side"> 
+		<div class="w3-dropdown-hover profile">
+        <img class="w3-circle w3-image w3-xlarge" src="head.png" style="width:64" height="64">
         <div class="w3-dropdown-content w3-bar-block w3-border">
-        <a href="profile.php?id=<?php echo $uid?>" class="w3-bar-item w3-button">個人檔案</a>
+        <a href="#" class="w3-bar-item w3-button">個人檔案</a>
         <a href="#" class="w3-bar-item w3-button">我的願望清單</a>
         </div>
         </div>
-    </div>
-  
-  <div class="fixed w3-container post" align="center">
-    <a href="post.php?id=<?php echo $uid?>"><button class="button" >POST</button></a>
-  </div>
-
-  <div class="container type" align="center">
-  
+</div>
 
 <div class="container group" align="center">
-       
+  	
 
-<div class="container group" align="center">
-       
-
-    <div class="btn-group btype" role="group">
+         
+    <div class="btn-group btype" role="group" align="center">
       <div class="btn-group" role="group">
-        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B1" data-toggle="dropdown" aria-expanded="false" >服飾<span class="caret"></span></button>
+        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B1" data-toggle		="dropdown" aria-expanded="false">服飾<span class="caret"></span></button>
         <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
-          <li><a onclick="select(1)">女生</a></li>
-          <li><a onclick="select(2)">男生</a></li>
+          <li><a href="index.php?clo">女生</a></li>
+          <li><a href="#">男生</a></li>
         </ul>
       </div>
       <div class="btn-group" role="group">
-        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B2" data-toggle="dropdown" aria-expanded="false" >鞋類<span class="caret"></span></button>
+        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B2" data-toggle="dropdown" aria-expanded="false">鞋類<span class="caret"></span></button>
         <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
-          <li> <a onclick="select(3)">女生</a></li>
-          <li> <a onclick="select(4)">男生</a></li>
+          <li><a href="#">女生</a></li>
+          <li><a href="#">男生</a></li>
         </ul>
       </div>
-      <button type="button" class="btn btn-default B3"  onclick="select(5)">生活用品</button>
-      <button type="button" class="btn btn-default B4" onclick="select(6)">3C</button>
-      <button type="button" class="btn btn-default B5" onclick="select(7)">書本</button>
-      <button type="button" class="btn btn-default B6" onclick="select(8)">文具</button>
+      <button type="button" class="btn btn-default B3">生活用品</button>
+      <button type="button" class="btn btn-default B4">3C</button>
+      <button type="button" class="btn btn-default B5">書本</button>
+      <button type="button" class="btn btn-default B6">文具</button>
     </div>
     
-        
+    
   </div>
  
   
@@ -181,16 +168,14 @@ div.desc { /* Add desc...區塊的大小*/
   
 </div>
 
- 
-
-
+<div class="fixed w3-container post" align="center">
+  <a href="post.php?id=<?php echo $uid?>"><button class="button" >POST</button></a>
+</div>
 </body>
 
 
 <script>
-function select(type){
-    document.location.href="index.php?type="+type;
-}
+
    
 </script>
 

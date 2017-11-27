8ba65543fe68b0c1e73014a820b949a14e456e79
shaohonglee <d38372002@gmail.com>
2017-11-27 14:39:29 +0800

    按讚數寫入object table

--- like.php
+++ like.php
@@ -19,15 +19,23 @@ else{
     	if(!$row=oci_fetch_row($stmt)){
     		$sql="INSERT INTO LIKERECORD (OID,MID,LID) VALUES ($oid,$mid,$liker)";   //無紀錄則增加
     		$stmt=oci_parse($db_link, $sql);
-    		oci_execute($stmt);
-    		exit;
+    		oci_execute($stmt); 		
     	}
     	else{
-    		$sql="DELETE FROM LIKERECORD WHERE OID=$oid AND MID=$mid";				//有紀錄則刪除
+    		$sql="DELETE FROM LIKERECORD WHERE OID=$oid AND MID=$mid AND LID=$liker";				//有紀錄則刪除
     		$stmt=oci_parse($db_link, $sql);
     		oci_execute($stmt);
-    		exit;
     	}
+        $sql="SELECT COUNT(*) From LIKERECORD WHERE OID=$oid and mid=$mid";
+        $stmt=oci_parse($db_link, $sql);
+        oci_execute($stmt);
+        $row=oci_fetch_row($stmt);
+        $likecount=$row[0];
+        echo $likecount;
+        $sql="UPDATE OBJECT SET OLIKE=$likecount WHERE OID=$oid and MID=$mid";
+        $stmt=oci_parse($db_link, $sql);
+        oci_execute($stmt);
+        exit;
 	}
 }
 	

acf20b1dc6aff73464ec134d4ee4ab94a5359180
zonaylc <33519861+zonaylc@users.noreply.github.com>
2017-11-27 14:59:23 +0800

    調整版型以及overflow
    個人檔案部分的overflow未完成

--- profile.php
+++ profile.php
@@ -14,28 +14,28 @@
   <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
   <script src="Select_Object_Content.js"></script>
   <script src="delete.js"></script>
-  <script src="update.js"></script>
 
 
 
 
 <style>
+.space{
+	border-spacing: 100% 100%;
+	overflow: hidden;
+}
 div.gallery {
-    margin: 40px;
-    border: 1px solid #ccc;
+    border: 5px solid #FCFCFC;
     float: left;
-    width: 250px;
-    height:250px;
+	margin:1%;
 }
 
 div.gallery:hover {
-    border: 10px solid #FFF;
+    border: 2px solid #FFF;
 }
 
 div.gallery img {
-    width: 240px;
-    height: 240px;
-    margin-top:5px;
+    width: 235px;
+    height: 235px;
 }
 
 div.desc { /* Add desc...區塊的大小*/
@@ -44,45 +44,23 @@ div.desc { /* Add desc...區塊的大小*/
 	background-color: #FCFCFC
 }
 #wall{
-    width:60%;
+    width:100%;
+	left:2%;
+	padding:100px;
+	margin-top: 20px;
+	border-spacing: 100% 100%;
+	overflow: hidden;
 }
 * {
     box-sizing: border-box;
 }
 
 .responsive {
-    padding: 0 6px;
+    padding: 0 4px;
     float: left;
     width: 30%;
 }
-.modal-backdrop{
-    background-color:white;
-    Z-index:0;
-}
-.modal{
-    background-color:rgba(199, 194, 194, 0.9);
-}
-.loader {
-  margin-left:150px;
-  border: 16px solid #f3f3f3;
-  border-radius: 50%;
-  border-top: 16px solid #3498db;
-  width: 120px;
-  height: 120px;
-  -webkit-animation: spin 2s linear infinite;
-  animation: spin 2s linear infinite;
-  
-}
 
-@-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
-}
-
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
 @media only screen and (max-width: 893px){
     .responsive {
         width: 49.99999%;
@@ -95,8 +73,12 @@ div.desc { /* Add desc...區塊的大小*/
         width: 100%;
     }
     .modal-content{
-        clear:left;
-        margin-right:100px;
+        clear:both;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		-ms-transform: translate(-50%, -50%);
+		transform: translate(-50%, -50%);		
     }
 }
 
@@ -109,46 +91,71 @@ div.desc { /* Add desc...區塊的大小*/
 </head>
 <body class="body">
 
-<div class="w3-container w3-blue-gray topbar">
-	<form action="">
-    <img src="title.png" alt="" width="198" height="84" class="tp" />
-  	<input id="search-box" type="text" name="search-box"/>
-    </form>
+<div class="w3-container w3-blue-gray topbar" align="left">
     
-<aside class="loginside" align="right">
-<?php
-
-  require 'Oracle_connect.php';
+    <div class="right">
+	<?php
+  	require 'Oracle_connect.php';
   
-  if(!isset($_SESSION['uname'])){//若使用者尚未登入
+  	if(!isset($_SESSION['uname'])){//若使用者尚未登入
       
 	  header('Location:login.php');
-  }
-  else{
+  		}
+  	else{
        $uid=$_SESSION['uid'];
 	   echo $_SESSION['uname'].'您好!'.'<a href="logout.php">登出</a>' ;
-       $id=$_GET['id'];//網址參數的id
-      
-       
-  }
-?>
-</aside>
+  		}
+	?>  
+	</div>
+    
+    <form action="search">
+    <img src="title.png" alt="" width="106" height="42" class="tp" />
+  	<input id="search-box" type="text" name="search-box"/>
+    <button class="button sb" >搜尋</button></a> 
+    </form>
+    
+    
+    <div class="container group" align="center">
+     <div class="btn-group btype" role="group" align="center">
+      <div class="btn-group" role="group">
+        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B1" data-toggle		="dropdown" aria-expanded="false">服飾<span class="caret"></span></button>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
+          <li><a href="#" onclick="select(1)">女生</a></li>
+          <li><a href="#" onclick="select(2)">男生</a></li>
+        </ul>
+      </div>
+      <div class="btn-group" role="group">
+        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B2" data-toggle="dropdown" aria-expanded="false">鞋類<span class="caret"></span></button>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
+          <li><a href="#" onclick="select(3)">女生</a></li>
+          <li><a href="#" onclick="select(4)">男生</a></li>
+        </ul>
+      </div>
+       <button type="button" class="btn btn-default B3"  onclick="select(5)">生活用品</button>
+      <button type="button" class="btn btn-default B4" onclick="select(6)">3C</button>
+      <button type="button" class="btn btn-default B5" onclick="select(7)">書本</button>
+      <button type="button" class="btn btn-default B6" onclick="select(8)">文具</button>
+    </div>
+   </div>
 </div>
 
 
-<div class="container" align="center">
-        <div class="container type" align="center">
-        <div class="container selfdesc">
-        <img src="head.png" alt="headsticker" id="head"/> <br>
+        <div class="container selfdesc" align="center" style="background:#E3DEB0">
+        <img src="head.png" alt="headsticker" id="headsticker" width="128" height="128"/>
             <div class="container desc-text" align="center">
             <?php
             echo $_SESSION['uname'];
-            ?><br>
-            我是李帥，很醜
+            ?>
+            <button class="edit">編輯個人檔案</button>
+            <br>
+            幾樣交換物品<br>
+            個人檔案敘述放在這裡
             </div> 
         </div>
          
           </div>
+		  
+		  
         <div id="wall" align="center">
               
         </div>
@@ -163,11 +170,7 @@ div.desc { /* Add desc...區塊的大小*/
   
   <div class="modal-content" id="text_area" style="">
     <img class="modal-content" id="img01" style="float:left"  >
-    <div id="img_info" style="background-color:white;width:1000px;height:558px"><?php if ($uid==$id) echo '<button id="edit_btn" type="button"  class="btn btn-default dropdown-toggle B1">編輯</button><div id="delete_btn" class="btn btn-default dropdown-toggle B1" data-toggle="dropdown" aria-expanded="false"  style="float:right;margin:10px" >...</div>' ;?> <div id="obj_cont"></div></div>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="delete_btn">
-          <li><a href="#" onclick="select(1)">女生</a></li>
-          <li><a href="#" onclick="select(2)">男生</a></li>
-        </ul>
+    <div id="img_info" style="background-color:white;width:1000px;height:558px"><?php if ($uid==$id) echo '<div id="delete_btn" style="float:right;margin:10px" >...</div>' ;?> </div>
   </div>
   
   
@@ -238,36 +241,7 @@ div.desc { /* Add desc...區塊的大小*/
   </div>
   
   
-  <!-- update Modal -->
-  <button id="show_edit_modal" type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal4" style="display:none">Open Modal</button>
-  <div class="modal fade" id="myModal4" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">編輯貼文</h4>
-        </div>
-        <div class="modal-body" id="delete_msq">
-          
-          物品名稱:<input type="text" id="obj_name">
-          <br>
-          內容:
-          <br>
-          <textarea rows="20" cols="40" placeholder="對這個物品寫點敘述吧" id="obj_dec" name="obj_dec"></textarea>
-                
-           
-		   <button onclick="edit_request()" style="float:right" type="button" class="btn btn-info"  id="img_submit">更改</button>
-        </div>
-        <div class="modal-footer">
-          <button id="edit_suc" type="button" class="btn btn-default" data-dismiss="modal" style="display:none">success</button>
-          
-        </div>
-      </div>
-      
-    </div>
-  </div>
+  
 
 
 
